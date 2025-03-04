ff2f1d5831e3ea59116b8db2159da388c0168d8c
DavidQF555 <davidfeng2003@gmail.com>
2025-03-04 11:54:43 -0800

    setup mod frame

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Hive](https://www.curseforge.com/minecraft/mc-mods/hive)
+
+A Minecraft mod that adds new mobs.
---
+++ src/main/java/hive/common/Hive.java
@@ -0,0 +1,19 @@
+package hive.common;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Hive.ID)
+public class Hive {
+
+    public static final String ID = "hive";
+
+    public Hive(IEventBus bus, ModContainer container) {
+        addRegistries(bus);
+    }
+
+    private void addRegistries(IEventBus bus) {
+    }
+
+}
